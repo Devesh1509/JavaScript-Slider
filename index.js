b384586f499716de151077967bc79a2670b51459
praptisharma28 <123169861+praptisharma28@users.noreply.github.com>
2023-10-04 20:56:42 +0530

    slider_functioning #1 issue resolved
    The loop is now working perfectly both the way, made new classic arrows too.
    Bug removed !
    Hope you like it.

--- index.js
+++ index.js
@@ -1,64 +1,28 @@
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-const slider = document.querySelector(".slider");
-const images = document.querySelectorAll(".image");
-const bottom = document.querySelector(".bottom");
+let slideIndex = 1;
+showSlides(slideIndex);
 
-let slideNumber = 1;
-const length = images.length;
 
-for (let i = 0; i < length; i++) {
-  const div = document.createElement("div");
-  div.className = "button";
-  bottom.appendChild(div);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-const buttons = document.querySelectorAll(".button");
-buttons[0].style.backgroundColor = "white";
 
-const resetBg = () => {
-  buttons.forEach((button) => {
-    button.style.backgroundColor = "transparent";
-  });
-};
-
-buttons.forEach((button, i) => {
-  button.addEventListener("click", () => {
-    resetBg();
-    slider.style.transform = `translateX(-${i * 800}px)`;
-    slideNumber = i + 1;
-    button.style.backgroundColor = "white";
-  });
-});
-
-const nextSlide = () => {
-  slider.style.transform = `translateX(-${slideNumber * 800}px)`;
-  slideNumber++;
-};
-const prevSlide = () => {
-  slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`;
-  slideNumber--;
-};
-const getFirstSlide = () => {
-  slider.style.transform = `translateX(0px)`;
-  slideNumber = 1;
-};
-const getLastSlide = () => {
-  slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
-  slideNumber = length;
-};
-
-const changeColor = ()=>{
-  resetBg()
-  buttons[slideNumber-1].style.backgroundColor = "white";
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-right.addEventListener("click", () => {
-  slideNumber < length ? nextSlide() : getFirstSlide();
-  changeColor()
-});
-
-left.addEventListener("click", () => {
-  slideNumber > 1 ? prevSlide() : getLastSlide();
-  changeColor()
-});
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
